55aa4ee31287191e8d4df63aa20efecec9397fd7
sanggusseu <sanggusseu@gmail.com>
2024-09-02 12:04:21 +0900

    Auto Page & Auto Layout 설정

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="text-h4">커뮤니티 목록</div>
+    <ul>
+      <li>
+        <router-link to="/posts/1">1번 게시글</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/mypage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="text-h4">MyPage</div>
+    <ul>
+      <li>
+        <router-link to="/mypage/profile">Profile</router-link>
+      </li>
+      <li>
+        <router-link to="/mypage/password">Password</router-link>
+      </li>
+      <li>
+        <router-link to="/mypage/bookmark">Bookmark</router-link>
+      </li>
+    </ul>
+    <router-view />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
