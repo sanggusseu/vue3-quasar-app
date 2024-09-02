e01e083aed0dec7756025ef57473bad70718d75d
sanggusseu <sanggusseu@gmail.com>
2024-09-02 15:14:08 +0900

    웹 레이아웃 구성 router-link vs router.push

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,66 @@
 <template>
-  <div>
-    <div class="text-h2">Default Layout</div>
-    <router-view />
-  </div>
+  <q-layout view="hHh lpR fFf" class="bg-grey-2">
+    <q-header bordered class="bg-white text-grey-9">
+      <q-toolbar>
+        <q-btn flat dense to="/">
+          <q-toolbar-title>
+            <q-avatar>
+              <!-- TODO: 아이콘 넣기 -->
+              <img
+                src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
+              />
+            </q-avatar>
+            짐코딩 클럽
+          </q-toolbar-title>
+        </q-btn>
+        <q-space />
+        <q-btn stretch flat label="Home" to="/home" />
+        <q-btn
+          stretch
+          flat
+          label="수강하기"
+          href="https://www.google.co.kr/?hl=ko"
+          target="_blank"
+        />
+        <q-btn
+          stretch
+          flat
+          label="온라인 강의"
+          href="https://edu.gymcoding.co/"
+          target="_blank"
+        />
+        <q-btn
+          stretch
+          flat
+          label="유튜브"
+          href="https://www.youtube.com/@gymcoding"
+          target="_blank"
+        />
+        <q-separator class="q-my-md q-mr-md" vertical />
+        <q-btn
+          unelevated
+          rounded
+          color="primary"
+          label="로그인 / 회원가입"
+        ></q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container :style="pageContainerStyles">
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 
-<style lang="scss" scoped></style>
+const route = useRoute();
+// console.dir(route);
+
+const pageContainerStyles = computed(() => ({
+  maxWidth: route.meta.width || `1080px`,
+  margin: `0 auto`,
+}));
+</script>
