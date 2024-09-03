ba8fd53a956a94c2472128b63e5d6896ae567f9c
sanggusseu <sanggusseu@gmail.com>
2024-09-03 11:59:07 +0900

    커뮤니티 목록 UI 컴포넌트 배치 및 정렬 친해지기

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,15 +2,27 @@
   <q-page padding>
     <div class="text-h4">커뮤니티 목록</div>
     <section class="q-gutter-y-sm q-mt-lg">
-      <q-card v-for="id in 100" :key="id">
-        <router-link :to="`posts/${id}`">
-          <q-card-section>{{ id }}번 게시글</q-card-section>
-        </router-link>
-      </q-card>
+      <PostList :items="posts" />
     </section>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import PostList from 'src/components/apps/post/PostList.vue';
+
+const posts = Array.from(Array(20), (_, index) => ({
+  id: index,
+  title: 'Vue3 Firebase 강의',
+  content:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi molestiae minima accusamus. Et vero repellendus sunt, facere, voluptatum id dignissimos repudiandae quas adipisci provident ipsa iusto qui eum, pariatur nostrum.',
+  readCount: 1,
+  commentCount: 2,
+  likeCount: 3,
+  bookmarkCount: 4,
+  tags: ['html', 'css', 'javascript'],
+  uid: 'uid',
+  category: '카테고리' + index,
+}));
+</script>
 
 <style lang="scss" scoped></style>
