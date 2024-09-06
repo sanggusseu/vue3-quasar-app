dc4004b9f78e4294f7abed067b6a02a11431902e
sanggusseu <sanggusseu@gmail.com>
2024-09-06 19:51:18 +0900

    extendRoutes 삭제

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,8 @@ import {
   createWebHistory,
   createWebHashHistory,
 } from 'vue-router/auto';
-import { routes } from 'vue-router/auto-routes';
-import { setupLayouts } from 'virtual:generated-layouts';
-// import routes from './routes';
+import { routes } from 'vue-router/auto-routes'; // 자동 생성된 라우트 가져오기
+import { setupLayouts } from 'virtual:generated-layouts'; // 레이아웃 설정
 
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
@@ -16,11 +15,11 @@ export default route(function (/* { store, ssrContext } */) {
     ? createWebHistory
     : createWebHashHistory;
 
+  // setupLayouts로 자동 생성된 라우트에 레이아웃 적용
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     history: createHistory(process.env.VUE_ROUTER_BASE),
-    routes,
-    extendRoutes: routes => setupLayouts(routes),
+    routes: setupLayouts(routes), // 자동 라우트에 레이아웃 설정
   });
 
   return Router;
